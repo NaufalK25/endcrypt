c71f5fbcb95a84617a82d12732e3728f2ed8bbb9
NaufalK25 <naufalkateni2001@gmail.com>
2022-11-26 17:16:38 +0700

    remove rail fence

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,6 +1,5 @@
 import caesarCrypto from './process/caesar';
 import vigenereCrypto from './process/vigenere';
-import railFenceCrypto from './process/rail-fence';
 
 const cryptoTypeInput = <HTMLInputElement>document.getElementById('crypto-type');
 
@@ -9,7 +8,6 @@ type CryptoViewAction = () => void;
 const CRYPTOS: { [key: string]: CryptoViewAction } = {
     caesar: caesarCrypto,
     vigenere: vigenereCrypto,
-    'rail-fence': railFenceCrypto
 };
 
 export default () => {
--- src/input.ts
+++ src/input.ts
@@ -1,6 +1,5 @@
 import { addInputElement as addCaesarInputElement, removeInputElement as removeCaesarInputElement } from './input/caesar';
 import { addInputElement as addvigenereInputElement, removeInputElement as removevigenereInputElement } from './input/vigenere';
-import { addInputElement as addRailFenceInputElement, removeInputElement as removeRailFenceInputElement } from './input/rail-fence';
 
 type InputElement = () => void;
 type AddInputElement = InputElement;
@@ -9,11 +8,9 @@ type RemoveInputElement = InputElement;
 export const ADD_INPUT_ELEMENTS: { [key: string]: AddInputElement } = {
     caesar: addCaesarInputElement,
     vigenere: addvigenereInputElement,
-    'rail-fence': addRailFenceInputElement
 };
 
 export const REMOVE_INPUT_ELEMENTS: { [key: string]: RemoveInputElement } = {
     caesar: removeCaesarInputElement,
     vigenere: removevigenereInputElement,
-    'rail-fence': removeRailFenceInputElement
 };
